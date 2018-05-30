6795a2ba6cf93b8793699253d1e4433802761de8
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-30 10:30:58 +0400

    conditionalop sample

---
+++ src/com/basic/conditionalop.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class conditionalop {
+	public static void main(String s[]) {
+	int a=10;
+	boolean b;
+		b=(a<=10)?true:false;
+		System.out.println(b);		
+	}
+	
+
+	
+
+}
