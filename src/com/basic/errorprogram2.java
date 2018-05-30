806bd74189904ea1eccf4de4416841f9e2113787
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-30 12:44:41 +0400

    errorprogram2 sample

---
+++ src/com/basic/errorprogram2.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class errorprogram2 {
+	public static void main(String args[]) {
+		int x = 1, total=0;
+		while ( x <= 10 )
+		{
+			total += x;
+			++x;
+			System.out.println(total);
+			}
+
+	}
+}
