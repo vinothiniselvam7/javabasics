95a86e6c9689919028b424cc713fb9864d4e0577
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-30 13:20:40 +0400

    errorprg4

---
+++ src/com/basic/errorprg4.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class errorprg4 {
+	public static void main (String args[]) {
+	 int y=1;
+	 while (y> 0 )
+	{
+	System.out.println(y);
+	++y;
+	break;
+	    
+
+	}
+	
+	}
+}
