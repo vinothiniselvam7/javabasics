f26b32adfa3f4aba26054bcc31ac219e02c53bb5
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-30 14:52:27 +0400

    condition prg

---
+++ src/com/basic/condition.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+public class condition {
+	public static void main(String args[]) {
+		int i,j,k;
+		for (i= 1; i <= 5; i++ )
+		{
+		for (j= 1; j <= 3; j++ )
+		{
+		for (k= 1; k <= 4; k++ )
+		System.out.print( '*' );
+		System.out.println();
+		} // end inner for
+		System.out.println();
+		} // end outer for
+
+
+		 
+
+	}
+
+}
