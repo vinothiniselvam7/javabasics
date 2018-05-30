d898abbded41626337f750042b085fd303f8c742
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-30 10:45:49 +0400

    sampe switchcase

---
+++ src/com/basic/switchcase.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+public class switchcase {
+	public static void main(String args[]) {
+		int a=10;
+		switch(a)
+		{
+		case 1:
+			System.out.println(100);
+			break;
+		case 2:
+			System.out.println(25);
+			 break;
+		default :
+			System.out.println("default");
+		}
+		
+
+		
+	}
+	
+}
