6d1964d17ba7bc956b0e2c8a4e3eefe3d8c133a5
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-30 09:11:30 +0400

    sample code

---
+++ src/com/basic/ifstatement.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class ifstatement {
+	public static void main(String args[]) {
+		int i=22;
+		if(i>35) {
+			System.out.println("pass");
+			
+		}
+		else
+		{
+	   System.out.println("fail");
+		}
+	}
+
+}
