39d2f8bfbce8c2dfad85dc8559c4bd0082c64bab
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-30 13:08:52 +0400

    errorprg sample

---
+++ src/com/basic/errorprogram3.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class errorprogram3 {
+	public static void main(String agrs[]) {
+		int x=1,total=0;
+		while ( x <= 100 ) {
+	   total += x;
+	   
+	   System.out.println(total);
+	   ++x;
+
+
+		
+	}
+
+}
+}
