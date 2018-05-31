d01f17e6687fb6a8474a90302ea9d1582fe125b7
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-31 12:50:03 +0400

    invoice sample code

---
+++ src/com/basic/Invoice.java
@@ -0,0 +1,59 @@
+package com.basic;
+
+public class Invoice {
+	String partNumber;
+	String partDescription;
+	int quantity;
+	double price;
+	public String getPartNumber() {
+		return partNumber;
+	}
+	public void setPartNumber(String partNumber) {
+		this.partNumber = partNumber;
+	}
+	public String getPartDescription() {
+		return partDescription;
+	}
+	public void setPartDescription(String partDescription) {
+		this.partDescription = partDescription;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		if(quantity<0)
+				{
+			this.quantity=0;
+				}
+		else {
+		this.quantity = quantity;
+		}
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		if(price<0)
+		 {
+			this.price=0.0;
+		}
+		else
+		{
+		this.price = price;
+		}
+	}
+	public double getInvoiceAmount() {
+		return price*quantity;
+		
+	}
+	public static void main(String args[]) {
+		Invoice Invoice1=new Invoice();
+		Invoice1.setQuantity(10);
+		Invoice1.setPrice(100);
+		System.out.println(Invoice1.getQuantity());
+		System.out.println(Invoice1.getPrice());
+		System.out.println(Invoice1.getInvoiceAmount());
+	}
+}
+	
+
