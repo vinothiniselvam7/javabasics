5e8f98d4e76c2c2ac98b4723fc7549c56ad22338
vinothiniselvam7 <vinothini191097@gmail.com>
2018-05-31 17:21:13 +0400

    even sample

---
+++ src/com/basic/isEven.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class isEven {
+	public static void main(String args[])
+	{
+		isEven t1=new isEven();
+	    System.out.print(t1.isEven1(4));
+	}
+	public boolean isEven1(int one)
+	{
+	return  one%2==0?true:false;
+	}
+	}
+
+
+
---
+++ src/com/basic/isMultiple.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class isMultiple {
+	public static void main(String args[]) {
+		
+				isMultiple t1=new isMultiple();
+			    System.out.print(t1.isMultiple1(2,3));
+			}
+		public boolean  isMultiple1 (int one,int two)
+		{
+			return two%one==0?true:false;
+		}
+		
+
+
+	}
+
+
