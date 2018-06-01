4ad161786c90debcc113e3d01bd42825857ff09b
vinothiniselvam7 <vinothini191097@gmail.com>
2018-06-01 08:59:38 +0400

    rectangle

---
+++ src/com/basic/Shape.java
@@ -0,0 +1,35 @@
+package com.basic;
+
+public class Shape {
+	double side;
+	double length;
+	double breadth;
+
+	public Shape(double side)
+	{
+	this.side=side;
+	}
+   public Shape(double length,double breadth)
+   {
+	   if( length<20&&length>0&&breadth<20&&breadth>0)
+	   {
+		   this.length=length;
+		   this.breadth=breadth;
+	   }else
+	   {
+		 System.out.println("the values should be from");
+	   }
+   }
+	public double findAreaofRec() {
+		return length*breadth;
+	}  
+	public double findAreaofSquare() {
+		return side*side;
+	}
+	public static void main(String args[]) {
+		Shape e1=new Shape(5);
+		Shape e2=new Shape(5,5);
+		System.out.println(e1.findAreaofSquare());	
+		System.out.println(e2.findAreaofRec());	
+	}
+}
--- src/com/basic/lvoice.java
+++
@@ -1,5 +0,0 @@
-package com.basic;
-
-public class lvoice {
-
-}
