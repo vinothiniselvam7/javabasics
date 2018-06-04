6badc4d5b0cadbc2ccf597e0c805b10b24843367
vinothiniselvam7 <vinothini191097@gmail.com>
2018-06-04 10:39:00 +0400

    DatatypePrimitve

---
+++ src/com/basic/DatatypePrimitve.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class DatatypePrimitve {
+ public static void main(String args[]) {
+	 int number;
+		String name;
+		double speed=51.5;
+		boolean isrunning;
+		System.out.println("speed "+speed);
+ }}
+
+	
---
+++ src/com/basic/EmptyCar.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class EmptyCar
+{
+	public EmptyCar() {
+		
+		System.out.println("empty constructor called");
+	}
+
+}
