bad6a63da311275f9b20fca8a9c254f865c1842f
vinothiniselvam7 <vinothini191097@gmail.com>
2018-06-04 13:04:22 +0400

    Operators

---
+++ src/com/basic/Operators.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Operators { 
+	
+  public static void main(String args[]) {
+	  int count=120;
+	  count++;
+	  count--;
+	  System.out.println(count!=250);
+	  System.out.println(count==200);
+  }
+
+}
