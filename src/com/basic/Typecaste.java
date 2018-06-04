b5513c809a62e0c4b79f6424fe2c764d9baf64a2
vinothiniselvam7 <vinothini191097@gmail.com>
2018-06-04 11:02:48 +0400

    Typecaste

---
+++ src/com/basic/Typecaste.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class Typecaste {
+   public static void main(String args[]) {
+	   byte one=12;
+	   byte two=16;
+	   int three =one+two;
+	   System.out.println("addition  of two numbers:"+three);
+	   
+   }
+}
