45e115455c7fcf98aa9f2a5105adaaa4e213ffdd
vinothiniselvam7 <vinothini191097@gmail.com>
2018-06-06 11:12:32 +0400

    sample code

---
+++ src/com/basic/Reverse.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class Reverse {
+	public static void main(String args[]) {
+ int[] array=new int[] {3,4,5,2,33,24,2 };
+ for(int i=array.length-1;i>=0;i--) {
+	 System.out.println(array[i]);
+	 
+ }
+}
+}
